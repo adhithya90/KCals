ccdec059f348f04a83bdda07e13f568b58e8307f
Adhithya Ramakumar <adhithya.ramakumar@gmail.com>
2022-11-16 22:26:31 -0800

    Added & changed fontFamily

--- app/src/main/java/com/example/kcals/ui/theme/Type.kt
+++ app/src/main/java/com/example/kcals/ui/theme/Type.kt
@@ -2,17 +2,35 @@ package com.example.kcals.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+
 // Set of Material typography styles to start with
+
+val figtree = FontFamily(
+    Font(com.example.core.R.font.figtreebold),
+    Font(com.example.core.R.font.figtreeblack),
+    Font(com.example.core.R.font.figreelight),
+    Font(com.example.core.R.font.figtreemedium),
+    Font(com.example.core.R.font.figtreeblack),
+    Font(com.example.core.R.font.figtreeregular)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = figtree,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = figtree,
+        fontWeight = FontWeight.Black,
+        fontSize = 32.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
