00a910fb825e4849c0202e19d4d8b36a2d76cbe4
Adhithya Ramakumar <adhithya.ramakumar@gmail.com>
2022-11-24 16:09:15 -0800

    Added navigation

--- app/src/main/java/com/example/kcals/MainActivity.kt
+++ app/src/main/java/com/example/kcals/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.core.navigation.Route
+import com.example.kcals.navigation.navigate
 import com.example.onboarding_presentation.WelcomeScreen
 import theme.KCalsTheme as theme
 
@@ -18,27 +24,64 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             theme() {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
                 ) {
-                    WelcomeScreen()
+                    composable(Route.WELCOME) {
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.VALUE_PROP1) {
+
+                    }
+                    composable(Route.VALUE_PROP2) {
+
+                    }
+                    composable(Route.AGE) {
+
+                    }
+                    composable(Route.GENDER) {
+
+                    }
+                    composable(Route.HEIGHT) {
+
+                    }
+                    composable(Route.WEIGHT) {
+
+                    }
+                    composable(Route.GOAL) {
+
+                    }
+                    composable(Route.NUTRIENT_GOAL) {
+
+                    }
+
+                    composable(Route.TRACKER_OVERVIEW) {
+
+                    }
+                    composable(Route.SEARCH) {
+
+                    }
+                    composable(Route.ACTIVITY) {
+
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-     theme {
-        WelcomeScreen()
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    theme {
+//        WelcomeScreen()
+//    }
+//}
--- onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/WelcomeScreen.kt
@@ -1,45 +1,50 @@
 package com.example.onboarding_presentation
 
-import android.content.res.Resources.Theme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.core.R
+import com.example.core.navigation.Route
+import com.example.core.util.UiEvent
 import com.example.core_ui.LocalSpacing
 import com.example.core_ui.components.ActionButton
-import theme.KCalsTheme as theme
-
 
 
 @Composable
-fun WelcomeScreen(){
+fun WelcomeScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit
+) {
     val spacing = LocalSpacing.current
-    Column(modifier = Modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center, 
-    horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = stringResource(id = R.string.welcome_text),
-        textAlign = TextAlign.Center, style = MaterialTheme.typography.h1)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            textAlign = TextAlign.Center, style = MaterialTheme.typography.h1
+        )
         Spacer(modifier = Modifier.height(spacing.med_spacing))
-        ActionButton(text = "Let's get started", onClick = { } )
+        ActionButton(text = "Let's get started",
+            onClick = {
+                onNavigate(UiEvent.Navigate(Route.VALUE_PROP1))
+            })
     }
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun WelcomeScreenPreview() {
-    theme() {
-        WelcomeScreen()
-    }
-
-
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun WelcomeScreenPreview() {
+//    theme() {
+//        WelcomeScreen(UiEvent.Navigate)
+//    }
+//
+//
+//}
 
