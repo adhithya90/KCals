c80b81168ee651bdcb8f3c605e71d6340261f99e
Adhithya Ramakumar <adhithya.ramakumar@gmail.com>
2022-11-26 21:54:40 -0800

    Added SelectableButton.kt & UnitTextField.kt

---
+++ core_ui/src/main/java/com/example/core_ui/components/SelectableButton.kt
@@ -0,0 +1,66 @@
+package com.example.core_ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.core_ui.LocalSpacing
+
+@Composable
+fun SelectableButton(
+    text: String,
+    isSelected: Boolean,
+    color: Color,
+    selectedTextColor: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.button
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(RoundedCornerShape(40.dp))
+            .border(
+                width = 2.dp,
+                color = color,
+                shape = RoundedCornerShape(LocalSpacing.current.small_spacing)
+            )
+            .background(
+                color = if (isSelected) color else Color.Transparent,
+                shape = RoundedCornerShape(LocalSpacing.current.small_spacing)
+            )
+            .clickable {
+                onClick()
+            }
+            .padding(LocalSpacing.current.large_spacing)
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = if (isSelected) selectedTextColor else color
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SelectableButtonPreview() {
+    SelectableButton(
+        text = "How is it going man!",
+        isSelected = true,
+        color = MaterialTheme.colors.secondary,
+        selectedTextColor = MaterialTheme.colors.onSecondary,
+        onClick = { /*TODO*/ })
+}
